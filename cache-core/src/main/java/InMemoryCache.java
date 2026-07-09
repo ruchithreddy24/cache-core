b0bf6fc17d439926b7c82dc703e8bca8fb54e92a
Ruchith Reddy <108124180+ruchithreddy24@users.noreply.github.com>
2026-07-09 12:34:06 +0530

    Major update 2
    a. User can insert values with multiple parameters now
    1. just key and value
    2. Key value and expiry time
    
    b. number of times a value is being retreived is being tracked and updated now ( Hit Count )

--- cache-core/src/main/java/InMemoryCache.java
+++ cache-core/src/main/java/InMemoryCache.java
@@ -4,15 +4,36 @@ public class InMemoryCache<K,V> implements Cache<K,V>{
 
     private final ConcurrentHashMap<K, CacheEntry<V>> storage = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        InMemoryCache<Integer, String> cache = new InMemoryCache<>();
+        cache.put(10, "Ruchith", 5000);
+        while (true) {
+            String value = cache.get(10);
 
+            if (value == null) {
+                System.out.println("Cache Expired!");
+                break;
+            }
+            System.out.println(value);
+            Thread.sleep(1000);   // Wait for 1 second
+
+        }
     }
+
+    // ************ PUT Statements **************//
 
     @Override
     public void put(K key, V value) {
         CacheEntry<V> entry = new CacheEntry<>(value);
         storage.put(key, entry);
     }
+    @Override
+    public void put(K key, V value, long expiry) {
+        CacheEntry<V> entry = new CacheEntry<>(value, expiry);
+        storage.put(key, entry);
+    }
+
+    // ************** REMOVE Statements **************//
 
     @Override
     public void remove(K key) {
@@ -24,14 +45,24 @@ public class InMemoryCache<K,V> implements Cache<K,V>{
         storage.clear();
     }
 
+    // ************** INSIGHTS Statements ***************//
+
     @Override
     public int size() {
         return storage.size();
     }
 
+    // ************** RETRIEVAL Statements **************//
+
     @Override
     public V get(K key) {
         CacheEntry<V> entry = storage.get(key);
+        if (System.currentTimeMillis() > entry.getExpiresAt()) {
+            storage.remove(key);
+            return null;
+        }
+        entry.IncreaseHitCount();
+        System.out.print(entry.getHitCount());
         return entry.getValue();
     }
 }
